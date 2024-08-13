bfb17e98bf91a765efcc5c795dc66f96edb92a33
nabiullahbenish <nabiullah_benash@yahoo.com>
2024-08-13 14:18:02 -0700

    the first line of code on the new project shot it

---
+++ src/Firstweek/FirstDay.java
@@ -0,0 +1,17 @@
+package Firstweek;
+
+public class FirstDay {
+
+
+    public static void main (String [] args) {
+
+        System.out.println("Welcome to 'JAVA Program' workshop");
+
+
+
+
+    }
+
+
+
+}
